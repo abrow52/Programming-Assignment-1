403a778c0bdde3270e0c03b74f548f4c3c1b4193
abrow52 <abrow314@kent.edu>
2024-11-01 15:47:30 -0400

    Update consumer.cpp

--- consumer.cpp
+++ consumer.cpp
@@ -1,50 +1,67 @@
-//file for the consumer
+// Alicia Brown
+// Assignment 1: Consumer File
+// Dr. Qiang Guan
+// 11/1/24
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include <pthread.h>
-#include <sempaphore.h>
+#include <semaphore.h>
 #include <unistd.h>
-#define maxItems
+#include <cstdlib>
+#include <sys/mman.h>
+#include "shared.hpp"
 
-extern int table[maxItems]
-extern int count;
+int main(int argc, char *argv[]) {
+    sleep(1); // make sure the producer runs first
 
-sem_t empty;
-sem_t full;
-pthread_mutex_t mutex;
+    int shmShared = shm_open(shmPath, O_RDWR, S_IRUSR | S_IWUSR);
+    if(shmShared == -1) {
+        std::cerr << "Waiting for shared memory to be created..." << std::endl;
+        sleep(1); // wait before retrying
+    }
 
-void* consumer(void* arg){
-  for (int i = 0; i < 10; i++){
-    sem_wait(&full);
-    pthread_mutex_lock(&mutex);
+    ftruncate(shmShared, sizeof(int));
 
-    //consumer an item
-    count--;
-    print("Consumed: ", table[count]);
+    sharedData *consumer = (sharedData*)(mmap(nullptr, sizeof(sharedData), PROT_READ | PROT_WRITE, MAP_SHARED, shmShared, 0));
+    if (consumer == MAP_FAILED) {
+        std::cerr << "ERROR IN MAPPING SHARED MEMORY: " << strerror(errno) << std::endl;
+        close(shmShared);
+        return 1;
+    }
 
-    pthread_mutex_unlock(&mutex);
-    sem_post(&empty);
+    // initializing semaphores + variables for buffer.
+    consumer->empty = sem_open("/empty_semaphore", 0);
+    consumer->full = sem_open("/full_semaphore", 0);
 
-    sleep(rand() % 2);
-  }
-  return NULL;
+    if (consumer->empty == SEM_FAILED || consumer->full == SEM_FAILED) {
+        std::cerr << "Error opening semaphores: " << strerror(errno) << std::endl;
+        munmap(consumer, sizeof(sharedData));
+        close(shmShared);
+        return 1;
+    }
+
+    // number of items consumed
+    for (int i = 0; i < 2; ++i) {
+        sleep(1);
+        sem_wait(consumer->full);
+        sem_wait(consumer->mutex);  // locking crit. section
+
+        std::cout << " " << std::endl;
+        std::cout << "Consumed : " << i << std::endl;
+
+        consumer->out = (consumer->out + 1) % maxItems;  // keep reiterating
+
+        sem_post(consumer->empty); // incrementing value
+        sem_post(consumer->mutex); // leaves crit. section open
+    }
+
+    if (munmap(consumer, sizeof(sharedData)) == -1) {
+         std::cerr << "Error unmapping shared memory: " << strerror(errno) << std::endl;
+    }
+
+    // cleanup
+    close(shmShared);
+    shm_unlink(shmPath);
+    return 0;
 }
-
-int main(){
-  pthread_t cons_thread;
-
-  sem_init(&empty, 0, maxItems);
-  sem_init(&full, 0, 0);
-  pthread_mutex_init(&mutex, NULL);
-
-  pthread_create(&cons_thread, NULL, consumer, NULL);
-  pthread_join(cons_thread, NULL);
-
-  sem_destroy(&empty);
-  sem_destroy(&full);
-  pthread_mutex_destroy(&mutex);
-  return 0;
-}
-
 
